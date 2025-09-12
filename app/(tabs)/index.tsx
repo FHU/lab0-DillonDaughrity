93c9bd64d34e00ce559eca8b686497609554c87f
CursedDomino <dillon.daughrity@outlook.com>
2025-09-12 15:01:21 -0500

    Update

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,6 +5,7 @@ import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 export default function HomeScreen() {
   return (
@@ -16,6 +17,13 @@ export default function HomeScreen() {
           style={styles.reactLogo}
         />
       }>
+
+        <DateTimePicker
+          value={new Date()}
+          mode="date"
+          display={Platform.OS === 'ios' ? 'spinner' : 'default'}
+        />
+
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">Welcome!</ThemedText>
         <HelloWave />
